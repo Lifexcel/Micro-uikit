f8bbd22d83be4e8a46acef9ee6b6891e58981122
Subash Chandra Sapkota <subashsapkota59@gmail.com>
2021-09-11 17:19:39 +0545

    feat: [MICRO-89] Member dashboard layout.Add dashboard responsive layout

--- src/widgets/Dashboard/Dashboard.tsx
+++ src/widgets/Dashboard/Dashboard.tsx
@@ -10,63 +10,108 @@ import {
   SearchIcon,
   ArrowDropDownIcon,
   ArrowDownIcon,
+  ArrowForwardIcon,
 } from "../../components/Svg/index";
 import { Profile } from "../Menu/types";
 import { Flex } from "../../components/Flex";
 import { Card } from "../../components/Card";
+// import useMatchBreakpoints from "../../hooks/useMatchBreakpoints";
 import { Button } from "../../components/Button";
 import { DataType, useTable, ColumnType } from "../../components/Table";
-
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
+
 interface Props {
   profile: Profile;
 }
 
-const DashboardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  color: ${({ theme }) => theme.colors.primary};
+const Page = styled.div`
+  margin: 10px auto;
+  max-width: 300px;
+
+  ${({ theme }) => theme.mediaQueries.xs} {
+    max-width: 350px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    max-width: 550px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    max-width: 850px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    max-width: 940px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.xl} {
+    max-width: 960px;
+  }
+  ${({ theme }) => theme.mediaQueries.xxl} {
+    max-width: 1180px;
+    margin: 0 0 0 10px;
+  }
 `;
+
 const Search = styled.div`
   display: flex;
   align-items: center;
   position: relative;
 `;
+
 const SearchButton = styled(Button)`
   position: absolute;
   right: 5px;
 `;
+
 const SearchInput = styled(Input)`
   border-radius: 5px;
   padding-right: 70px;
   box-shadow: none;
-  width: 400px;
+  width: 100%;
+  ${({ theme }) => `${theme.mediaQueries.md} {
+   width:300px;
+  }`}
 `;
 const DashboardHeader = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 5px;
+
+  ${({ theme }) => `${theme.mediaQueries.md} {
+    flex-direction:row;
+    justify-content:space-between;
+  }`}
 `;
 const MainBar = styled.div`
-   display:block:
-   align-items:center;
-   justify-content:center;
-   margin:10px 5px;
-   box-shadow:${({ theme }) => `1px 1px 2px ${theme.colors.tertiary}`};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-shadow: ${({ theme }) => `1px 1px 5px 0px ${theme.colors.tertiary}`};
+  margin-bottom: 10px;
+  padding: 0 10px;
+  border-radius: 5px;
+
+  ${({ theme }) => `${theme.mediaQueries.md} {
+    flex-direction:row;
+    justify-content:space-between;
+  }`}
 `;
+
 const StyledAvatar = styled.img`
-  width: 100px;
-  height: 100px;
+  width: 70px;
+  height: 70px;
   object-fit: cover;
   border-radius: 50%;
 `;
 const Address = styled.div``;
+
 const Container = styled.div`
   border: ${({ theme }) => `1px solid ${theme.colors.tertiary}`};
   background-color: ${({ theme }) => theme.colors.tertiary};
   border-radius: 5px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  display: inline-block;
   margin: 0 1rem 0 0;
 
   img {
@@ -96,8 +141,9 @@ const StyledCard = styled(Card)`
 
 const DashboardMain = styled.div`
   display: flex;
-  justifycontent: space-around;
-  alignitems: flex-start;
+  align-items: center;
+  flex-direction: column;
+  width: inherit;
 
   .portfolio-container {
     height: 300px;
@@ -105,24 +151,59 @@ const DashboardMain = styled.div`
     justify-content: center;
     align-items: center;
     margin: 10px 0;
-
     border: 2px solid ${({ theme }) => theme.colors.tertiary};
     border-radius: 5px;
   }
+
   .content-right {
-    width: 27%;
+    width: 100%;
   }
+
   .content-left {
-    width: 70%;
     margin: 0px 10px 10px 0;
+    width: inherit;
+
+    ${({ theme }) => theme.mediaQueries.xs} {
+      max-width: 350px;
+    }
+
+    ${({ theme }) => theme.mediaQueries.sm} {
+      max-width: 570px;
+    }
+
+    ${({ theme }) => theme.mediaQueries.md} {
+      max-width: 800px;
+    }
+
+    ${({ theme }) => theme.mediaQueries.lg} {
+      max-width: 940px;
+    }
+
+    ${({ theme }) => theme.mediaQueries.xl} {
+      max-width: 65%;
+    }
 
     .token-description-container {
       overflow-x: scroll;
+
       &::-webkit-scrollbar {
         display: none;
       }
     }
   }
+
+  ${({ theme }) => `${theme.mediaQueries.xl} {
+        flex-direction:row; 
+        align-items:flex-start;
+        
+        .content-right{
+            width:100%;
+            max-width:400px;
+        }
+        .content-left{
+            width:65%;
+        }
+    }`}
 `;
 const StyledAssetTable = styled.table`
   width: 100%;
@@ -151,22 +232,24 @@ const StyledAssetIcon = styled.img`
 const TokenDesc: React.FC = () => {
   return (
     <Container className="img-desc-item">
-      <img
-        src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201905/tiger_shroff_0.png?qMIrZn27cP8pqFXpOXoDacJphGoxa_3c&size=770:433"
-        width="50px"
-        height="50px"
-        alt="token"
-      />
-      <Flex className="description" flexDirection="column" justifyContent="space-around">
-        <Text>Title</Text>
-        <Flex alignItems="baseline">
-          <Text fontSize="18px" className="value" bold>
-            1,243,423
-          </Text>
-          <Text fontSize="14px" ml="1">
-            {" "}
-            MICRO
-          </Text>
+      <Flex>
+        <img
+          src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201905/tiger_shroff_0.png?qMIrZn27cP8pqFXpOXoDacJphGoxa_3c&size=770:433"
+          width="50px"
+          height="50px"
+          alt="token"
+        />
+        <Flex className="description" flexDirection="column" justifyContent="space-around">
+          <Text>Title</Text>
+          <Flex alignItems="baseline">
+            <Text fontSize="18px" className="value" bold>
+              1,243,423
+            </Text>
+            <Text fontSize="14px" ml="1">
+              {" "}
+              MICRO
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
     </Container>
@@ -177,9 +260,14 @@ const TransactionsTable = <T extends DataType>({ _columns, _data }: { _columns: 
   const { headers, rows } = useTable(_columns, _data, {
     sortable: true,
   });
+  const TransactionContaner = styled.div`
+    width: 100%;
+    overflow-x: scroll;
+  `;
 
   const StyledTable = styled.table`
     width: 100%;
+
     th {
       font-size: 16px;
       &:first-child {
@@ -211,34 +299,39 @@ const TransactionsTable = <T extends DataType>({ _columns, _data }: { _columns: 
   `;
 
   return (
-    <div>
-      <Flex justifyContent="space-between">
+    <>
+      <Flex justifyContent="space-between" alignItems="center">
         <Text>Transactions</Text>
-        <Text>All</Text>
+        <Flex>
+          <Text>All</Text>
+          <ArrowForwardIcon />{" "}
+        </Flex>
       </Flex>
-      <StyledTable>
-        <thead>
-          <tr>
-            {headers.map((header) => (
-              <th key={`header-${header?.name}`} data-testid={`column-${header.name}`}>
-                {header.label}
+      <TransactionContaner>
+        <StyledTable>
+          <thead>
+            <tr>
+              {headers.map((header) => (
+                <th key={`header-${header?.name}`} data-testid={`column-${header.name}`}>
+                  {header.label}
 
-                {header.sorted && header.sorted.on ? <span data-testid={`sorted-${header.name}`} /> : null}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row) => (
-            <tr data-testid={`row-${row.id}`} key={row.id}>
-              {row.cells.map((cell) => (
-                <td>{cell.render()}</td>
+                  {header.sorted && header.sorted.on ? <span data-testid={`sorted-${header.name}`} /> : null}
+                </th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </StyledTable>
-    </div>
+          </thead>
+          <tbody>
+            {rows.map((row) => (
+              <tr data-testid={`row-${row.id}`} key={row.id}>
+                {row.cells.map((cell) => (
+                  <td>{cell.render()}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </StyledTable>
+      </TransactionContaner>
+    </>
   );
 };
 
@@ -262,9 +355,9 @@ export const Dashboard: React.FC<Props> = ({ profile }) => {
     },
   ];
   return (
-    <DashboardWrapper className="dashboard">
+    <Page>
       <DashboardHeader className="dashboard-header">
-        <Flex flexDirection="row">
+        <Flex>
           <Flex flexDirection="column">
             <StyledAvatar src={profile?.image} alt="user" />
             <Text>
@@ -278,7 +371,7 @@ export const Dashboard: React.FC<Props> = ({ profile }) => {
         <Flex justifyContent="flex-start" alignItems="flex-start">
           <Address>
             <Text>
-              231231k23l1k3l123l12k3
+              k3l123l12k3
               <OpenNewIcon fontSize="14px" />
             </Text>
           </Address>
@@ -290,93 +383,94 @@ export const Dashboard: React.FC<Props> = ({ profile }) => {
           </Button>
         </Flex>
       </DashboardHeader>
-      <div className="dashboard-main">
-        <MainBar className="dashboard-main-bar">
-          <Flex justifyContent="space-between">
-            <Search>
-              <SearchInput type="text" inputMode="text" placeholder="Search for tokens pools and vaults" />
-              <SearchButton size="sm" variant="text">
-                <SearchIcon />
-              </SearchButton>
-            </Search>
-            <Flex justifyContent="space-between">
-              <Button variant="text">
-                63
-                <ArrowDownIcon />
-              </Button>
+      <MainBar className="dashboard-main-bar">
+        <Search>
+          <SearchInput type="text" inputMode="text" placeholder="Search for tokens pools and vaults" />
+          <SearchButton size="sm" variant="text">
+            <SearchIcon />
+          </SearchButton>
+        </Search>
+        <Flex>
+          <Button variant="text" mr="10px">
+            63
+            <ArrowDownIcon />
+          </Button>
 
-              <Button variant="text">
-                USD
-                <ArrowDropDownIcon />
-              </Button>
-            </Flex>
+          <Button variant="text">
+            USD
+            <ArrowDropDownIcon />
+          </Button>
+        </Flex>
+      </MainBar>
+      <DashboardMain className="dashboard-main-content">
+        <div className="content-left">
+          <Flex className="token-description-container" justifyContent="space-between">
+            <TokenDesc />
+            <TokenDesc />
+            <TokenDesc />
+            <TokenDesc />
+            <TokenDesc />
+            <TokenDesc />
+            <TokenDesc />
           </Flex>
-        </MainBar>
-        <DashboardMain className="dashboard-main-content">
-          <div className="content-left">
-            <Flex className="token-description-container" justifyContent="space-between">
-              <TokenDesc />
-              <TokenDesc />
-              <TokenDesc />
-              <TokenDesc />
-            </Flex>
-            <Flex className="portfolio-container">Please hold on we are gathering your portfolio</Flex>
-            <TransactionsTable _columns={transactionColumns} _data={transactionData} />
-          </div>
-          <div className="content-right">
-            <TransactButtons justifyContent="space-around">
-              <Button variant="success" size="sm">
-                Receive
-              </Button>
-              <Button variant="danger" size="sm">
-                Send
-              </Button>
-            </TransactButtons>
-            <StyledCard>
-              <Text fontSize="24px"> Binance Smart Chain</Text>
-              <Text fontSize="14px">Total Value</Text>
-              <Text fontSize="16px" bold>
-                $8,909.00
-              </Text>
-              <div>
-                <Text fontSize="16px"> Assets</Text>
-                <StyledAssetTable className="assets-table">
-                  <tr>
-                    <td>
-                      <Flex alignItems="center">
-                        <StyledAssetIcon src="https://cryptologos.cc/logos/binance-coin-bnb-logo.png" alt="asset" />{" "}
-                        0.01 BNB
-                      </Flex>
-                    </td>
-                    <td>$5.01</td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <Flex alignItems="center">
-                        <StyledAssetIcon src="https://cryptologos.cc/logos/binance-coin-bnb-logo.png" alt="asset" />{" "}
-                        0.01 BNB
-                      </Flex>
-                    </td>
-                    <td>$5.01</td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <Flex alignItems="center">
-                        <StyledAssetIcon src="https://cryptologos.cc/logos/binance-coin-bnb-logo.png" alt="asset" />{" "}
-                        0.01 BNB
-                      </Flex>
-                    </td>
-                    <td>$5.01</td>
-                  </tr>
-                </StyledAssetTable>
-              </div>
-              <Button variant="primary" size="sm" mt="2" type="button">
-                Add Token
-              </Button>
-            </StyledCard>
-          </div>
-        </DashboardMain>
-      </div>
-    </DashboardWrapper>
+          <Flex className="portfolio-container">
+            <Text>Please hold on we are gathering your portfolio</Text>
+          </Flex>
+          <TransactionsTable _columns={transactionColumns} _data={transactionData} />
+        </div>
+        <div className="content-right">
+          <TransactButtons justifyContent="space-around">
+            <Button variant="success" size="sm">
+              Receive
+            </Button>
+            <Button variant="danger" size="sm">
+              Send
+            </Button>
+          </TransactButtons>
+          <StyledCard>
+            <Text fontSize="24px"> Binance Smart Chain</Text>
+            <Text fontSize="14px">Total Value</Text>
+            <Text fontSize="16px" bold>
+              $8,909.00
+            </Text>
+            <div>
+              <Text fontSize="16px"> Assets</Text>
+              <StyledAssetTable className="assets-table">
+                <tr>
+                  <td>
+                    <Flex alignItems="center">
+                      <StyledAssetIcon src="https://cryptologos.cc/logos/binance-coin-bnb-logo.png" alt="asset" /> 0.01
+                      BNB
+                    </Flex>
+                  </td>
+                  <td>$5.01</td>
+                </tr>
+                <tr>
+                  <td>
+                    <Flex alignItems="center">
+                      <StyledAssetIcon src="https://cryptologos.cc/logos/binance-coin-bnb-logo.png" alt="asset" /> 0.01
+                      BNB
+                    </Flex>
+                  </td>
+                  <td>$5.01</td>
+                </tr>
+                <tr>
+                  <td>
+                    <Flex alignItems="center">
+                      <StyledAssetIcon src="https://cryptologos.cc/logos/binance-coin-bnb-logo.png" alt="asset" /> 0.01
+                      BNB
+                    </Flex>
+                  </td>
+                  <td>$5.01</td>
+                </tr>
+              </StyledAssetTable>
+            </div>
+            <Button variant="primary" size="sm" mt="2" type="button">
+              Add Token
+            </Button>
+          </StyledCard>
+        </div>
+      </DashboardMain>
+    </Page>
   );
 };
